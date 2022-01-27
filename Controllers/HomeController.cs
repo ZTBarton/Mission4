fe8bb9c5ce158a46635899ad28ef34117eb44f17
Zachary Barton <75874562+ZTBarton@users.noreply.github.com>
2022-01-26 17:39:46 -0700

    Initial check-in of module Mission4

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission4.Models;
+
+namespace Mission4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private movieContext _movieContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, movieContext movieCtx)
+        {
+            _logger = logger;
+            _movieContext = movieCtx;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcasts()
+        {
+            return View("podcasts");
+        }
+
+        [HttpGet]
+        public IActionResult SubmitMovie()
+        {
+            return View("submitMovie");
+        }
+
+        [HttpPost]
+        public IActionResult SubmitMovie(movieTemplate model)
+        {
+            _movieContext.Add(model);
+            _movieContext.SaveChanges();
+            return View("confirmation", model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/movieContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mission4.Models
+{
+    public class movieContext : DbContext
+    {
+        public movieContext(DbContextOptions<movieContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<movieTemplate> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<movieTemplate>().HasData(
+
+                    new movieTemplate
+                    {
+                        MovieID = 1,
+                        Category = "Action",
+                        Title = "Inception",
+                        Year = 2010,
+                        Director = "Christopher Nolan",
+                        Rating = "PG-13",
+                        Edited = false,
+                        Notes = "Mind-blowing"
+
+                    },
+                    new movieTemplate
+                    {
+                        MovieID = 2,
+                        Category = "Family",
+                        Title = "Coco",
+                        Year = 2017,
+                        Director = "Lee Unkrich",
+                        Rating = "PG",
+                        Edited = false,
+                        LentTo = "Carol",
+                        Notes = "Watch in spanish."
+                    },
+                    new movieTemplate
+                    {
+                        MovieID = 3,
+                        Category = "Historical",
+                        Title = "Jojo Rabbit",
+                        Year = 2019,
+                        Director = "Taika Waititi",
+                        Rating = "PG-13",
+                        Edited = false
+                    }
+                );
+        }
+    }
+}
