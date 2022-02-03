2166ff7271d17593a553f9a451356373d6f1d4a8
Zachary Barton <75874562+ZTBarton@users.noreply.github.com>
2022-02-02 19:42:56 -0700

    * movieContext.cs: * movieTemplate.cs: * _Layout.cshtml: * movieContextModelSnapshot.cs:
    * submitMovie.cshtml: More mission 5 functionality

--- Models/movieContext.cs
+++ Models/movieContext.cs
@@ -11,15 +11,29 @@ namespace Mission4.Models
         }
 
         public DbSet<movieTemplate> Movies { get; set; }
-
+        public DbSet<Category> Categories { get; set; }
+ 
         protected override void OnModelCreating(ModelBuilder mb)
         {
+
+            mb.Entity<Category>().HasData(
+                    new Category { CategoryId=1, CategoryName="Action/Adventure"},
+                    new Category { CategoryId = 2, CategoryName = "Historical" },
+                    new Category { CategoryId = 3, CategoryName = "Comedy" },
+                    new Category { CategoryId = 4, CategoryName = "Drama" },
+                    new Category { CategoryId = 5, CategoryName = "Family" },
+                    new Category { CategoryId = 6, CategoryName = "Horror/Suspense" },
+                    new Category { CategoryId = 7, CategoryName = "Television" },
+                    new Category { CategoryId = 8, CategoryName = "VHS" },
+                    new Category { CategoryId = 9, CategoryName = "Miscellaneous" }
+                );
+
             mb.Entity<movieTemplate>().HasData(
 
                     new movieTemplate
                     {
                         MovieID = 1,
-                        Category = "Action",
+                        CategoryId = 1,
                         Title = "Inception",
                         Year = 2010,
                         Director = "Christopher Nolan",
@@ -31,7 +45,7 @@ namespace Mission4.Models
                     new movieTemplate
                     {
                         MovieID = 2,
-                        Category = "Family",
+                        CategoryId = 5,
                         Title = "Coco",
                         Year = 2017,
                         Director = "Lee Unkrich",
@@ -43,7 +57,7 @@ namespace Mission4.Models
                     new movieTemplate
                     {
                         MovieID = 3,
-                        Category = "Historical",
+                        CategoryId = 2,
                         Title = "Jojo Rabbit",
                         Year = 2019,
                         Director = "Taika Waititi",
--- Models/movieTemplate.cs
+++ Models/movieTemplate.cs
@@ -9,18 +9,20 @@ namespace Mission4.Models
         [Required]
         public int MovieID { get; set; }
         [Required]
-        public string Category { get; set; }
-        [Required]
         public string Title { get; set; }
         [Required]
         public int Year { get; set; }
         [Required]
-        public string Director { get; set; }
+        public string Director { get; set; } 
         [Required]
         public string Rating { get; set; }
         public bool Edited { get; set; }
         public string LentTo { get; set; }
         [MaxLength(25)]
         public string Notes { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
